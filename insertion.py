65e3bb696a2d29e5b3eb972ba78a60bfb64a9ade
Milad <30826499+miladmyled@users.noreply.github.com>
2020-05-31 12:37:57 -0700

    Delete insertion.py

--- insertion.py
+++
@@ -1,9 +0,0 @@
-# Login to AWS
-conn = login.awslogin.conn
-cursor = conn.cursor
-
-
-sql = "INSERT INTO followings (profile_name, href , updated_date , is_followed) VALUES (%s, %s, %s, %s)"
-val = (profile_name, profile_address, datetime.now(), 1)
-cursor.execute(sql, val)
-conn.commit()
