93fa9ff909b8745066a5ab6bc5da24bdb6d7e966
Milad Firouzi <mfirouzi@GatewayCasinos.com>
2020-06-10 11:57:13 -0700

    save following targets in database

--- main.py
+++ main.py
@@ -125,24 +125,25 @@ for fhrf in finalhreflist:
                 "//div[@id='react-root']/section/main/div[1]/header/section/ul/li[3]/a/span").text)
         except :
             follower_count = 1001
-            follower_count = 1001
+            following_count = 1001
         sleep(2)
-        if follower_count < 1000 and following_count < 1000:
+        if follower_count < 1000:
             try:
                 follow_btn = login.browser.find_element_by_xpath(
                     "//div[@id='react-root']/section/main/div[1]/header/section/div[1]/div[1]/span/span/button")
                 profile_name = login.browser.find_element_by_xpath(
                     "//div[@id='react-root']/section/main/div[1]/header/section/div[1]/h2").text
+
                 sql = "INSERT INTO followings (profile_name, href , updated_date , is_followed) VALUES (%s, %s, %s, %s)"
-                val = (profile_name, profile_address, datetime.now(), 1)
+                val = (profile_name, profile_address, datetime.now(), 0)
                 with conn.cursor() as cursor:
                     cursor.execute(sql, val)
                 conn.commit()
-                follow_btn.click()
+                #follow_btn.click()
             except Exception as e:
                 print(e)
                 logerror("follow not available" , e)
-        elif following_count >= 1000:
+        elif follower_count >= 1000:
             try:
                 followers_btn = login.browser.find_element_by_xpath(
                     "//div[@id='react-root']/section/main/div[1]/header/section/ul/li[2]/a")
@@ -150,20 +151,29 @@ for fhrf in finalhreflist:
                 for a in range(20):
                     # for a in range(5):
                     try:
-                        login.browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+                        fBody = login.browser.find_element_by_xpath("//div[@class='isgrP']")
+                        login.browser.execute_script(
+                            'arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;',
+                            fBody)
                         sleep(2)
+                        for f in range(100):
+                            profile_name = login.browser.find_element_by_xpath(
+                                "//div[@role='dialog']/div[1]/div[2]/ul/div[1]/li[" + str(f + 1) + "]/div[1]/div[1]/div[2]/div[1]/a").text
+                            href = 'https://www.instagram.com/' + profile_name
+                            with conn.cursor() as cursor:
+                                cursor.execute(
+                                    "select (1) from followings WHERE href LIKE  '%" + profile_name + "%' limit 1")
+                            msg = cursor.fetchone()
+                            # check if it is empty
+                            if not msg:
+                                sql = "INSERT INTO followings (profile_name, href , updated_date , is_followed) VALUES (%s, %s, %s, %s)"
+                                val = (profile_name, href, datetime.now(), 0)
+                                with conn.cursor() as cursor:
+                                    cursor.execute(sql, val)
+                                conn.commit()
                     except Exception as e:
                         print(e)
                         logerror("Scroll down followers section", e)
-                #for f in range(20)
-                profile_name = login.browser.find_element_by_xpath(
-                    "//div[@id='react-root']/section/main/div[1]/header/section/div[1]/h2").text
-                sql = "INSERT INTO followings (profile_name, href , updated_date , is_followed) VALUES (%s, %s, %s, %s)"
-                val = (profile_name, profile_address, datetime.now(), 1)
-                with conn.cursor() as cursor:
-                    cursor.execute(sql, val)
-                conn.commit()
-                follow_btn.click()
             except Exception as e:
                 print(e)
                 logerror("follow not available" , e)
--- test.py
+++ test.py
@@ -20,78 +20,33 @@ try:
 except Exception as e:
     logerror("instalogin", e)
 
-finalhreflist = []
-with open('result.txt') as my_file:
-    for line in my_file:
-        line = line[:-2]
-        finalhreflist.append(line)
 
-
-# Open each post on final list , go to the profile check if the profile exists in database or not , if not
-# follow it and save profile name , href and datetime in the database
-for fhrf in finalhreflist:
-    login.browser.get(fhrf)
-    sleep(2)
+login.browser.get('https://www.instagram.com/capsliquor/')
+followers_btn = login.browser.find_element_by_xpath(
+                    "//div[@id='react-root']/section/main/div[1]/header/section/ul/li[2]/a")
+followers_btn.click()
+for a in range(20):
+    # for a in range(5):
     try:
-        profile_address = login.browser.find_element_by_xpath(
-            "//div[@id='react-root']/section/main/div[1]/div[1]/article/header/div[2]/div[1]/div[1]/a").get_attribute(
-            "href")
-        with conn.cursor() as cursor:
-            cursor.execute('select (1) from followings where href = "' + profile_address + '" limit 1')
+        fBody = login.browser.find_element_by_xpath("//div[@class='isgrP']")
+        login.browser.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;',                                     fBody)
+        sleep(2)
     except Exception as e:
         print(e)
-        logerror("profile address not found" , e)
+        logerror("Scroll down followers section", e)
+for f in range(100):
+    profile_name = login.browser.find_element_by_xpath(
+        "//div[@role='dialog']/div[1]/div[2]/ul/div[1]/li[" + str(f + 1) + "]/div[1]/div[1]/div[2]/div[1]/a").text
+    href = 'https://www.instagram.com/' + profile_name
+    with conn.cursor() as cursor:
+        cursor.execute(
+            "select (1) from followings WHERE href LIKE  '%" + profile_name + "%' limit 1")
     msg = cursor.fetchone()
     # check if it is empty
     if not msg:
-        login.browser.get(profile_address)
-        try:
-            follower_count = int(login.browser.find_element_by_xpath(
-                "//div[@id='react-root']/section/main/div[1]/header/section/ul/li[2]/a/span").text)
-            following_count = int(login.browser.find_element_by_xpath(
-                "//div[@id='react-root']/section/main/div[1]/header/section/ul/li[3]/a/span").text)
-        except :
-            follower_count = 1001
-            following_count = 1001
-        sleep(2)
-        if follower_count < 1000 :
-            try:
-                follow_btn = login.browser.find_element_by_xpath(
-                    "//div[@id='react-root']/section/main/div[1]/header/section/div[1]/div[1]/span/span/button")
-                profile_name = login.browser.find_element_by_xpath(
-                    "//div[@id='react-root']/section/main/div[1]/header/section/div[1]/h2").text
-                sql = "INSERT INTO followings (profile_name, href , updated_date , is_followed) VALUES (%s, %s, %s, %s)"
-                val = (profile_name, profile_address, datetime.now(), 1)
-                with conn.cursor() as cursor:
-                    cursor.execute(sql, val)
-                conn.commit()
-                follow_btn.click()
-            except Exception as e:
-                print(e)
-                logerror("follow not available" , e)
-        elif following_count >= 1000:
-            try:
-                followers_btn = login.browser.find_element_by_xpath(
-                    "//div[@id='react-root']/section/main/div[1]/header/section/ul/li[2]/a")
-                followers_btn.click()
-                for a in range(20):
-                    # for a in range(5):
-                    try:
-                        login.browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-                        sleep(2)
-                    except Exception as e:
-                        print(e)
-                        logerror("Scroll down followers section", e)
-                #for f in range(20)
-                profile_name = login.browser.find_element_by_xpath(
-                    "//div[@id='react-root']/section/main/div[1]/header/section/div[1]/h2").text
-                sql = "INSERT INTO followings (profile_name, href , updated_date , is_followed) VALUES (%s, %s, %s, %s)"
-                val = (profile_name, profile_address, datetime.now(), 1)
-                with conn.cursor() as cursor:
-                    cursor.execute(sql, val)
-                conn.commit()
-                follow_btn.click()
-            except Exception as e:
-                print(e)
-                logerror("follow not available" , e)
+        sql = "INSERT INTO followings (profile_name, href , updated_date , is_followed) VALUES (%s, %s, %s, %s)"
+        val = (profile_name, href, datetime.now(), 0)
+        with conn.cursor() as cursor:
+            cursor.execute(sql, val)
+        conn.commit()
 
